9fe2e39ca1de942a74b8b0191a7fdb1072e45607
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2020-08-05 14:43:32 +0200

    add build after deploy

---
+++ public/precache-manifest.709cdeb40bfb26bbaf72f493822919a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39573fa153e9de5b344845c67647c96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73bdc7079a533ad4e180",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "c03408a30ba6161ff8a5",
+    "url": "/static/css/main.c707ce55.chunk.css"
+  },
+  {
+    "revision": "73bdc7079a533ad4e180",
+    "url": "/static/js/2.ff4b40eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff4b40eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03408a30ba6161ff8a5",
+    "url": "/static/js/main.1e7feadd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
